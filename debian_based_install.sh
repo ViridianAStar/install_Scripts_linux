edce461f227bf3905f123558460fd0238209d89c
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2023-08-24 19:47:52 -0400

    Update debian_based_install.sh

--- debian_based_install.sh
+++ debian_based_install.sh
@@ -75,7 +75,6 @@ wait
 #apt-get install steam-installer
 #wait
 #for 32 bit titles additional 32 bit libraries will need to be installed
-#wait
 #apt-get install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 #wait
 #steam insaller installs here and you will need to run steam from here  ~/.steam/debian-installation/steam.sh
